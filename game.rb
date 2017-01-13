92a10a5aba42f5970d75a5914a7aa939ac675449
OOuissem <smartmen0909@gmail.com>
2017-01-13 14:36:08 +0100

    add principal program

--- game.rb
+++ game.rb
@@ -8,8 +8,21 @@ puts bridge
 
 animaux = ['chat', 'ours', 'renard', 'lapin']
 puts animaux
-# Ouissam
-finish="igloo"
+
+# Ouissam - vérifier iglou
+
+def check_home board
+    if board[igloo] == 6
+        puts("partie gagnée")
+        return "win"
+    elsif board[:bridge][pilars] == 0        
+        puts("partie perdu")
+        return "lost"
+    else
+        puts("Partie en cours, lancée suivante")
+        return "relaunch"
+    end
+end
 
 # Johann
 
@@ -21,4 +34,24 @@ end
 puts "Votre dé affiche: #{shoot}"
 
 # Patrick
+
+#Ouissem - principal
+def init 
+    board={field: {animals: 4} , bridge: {animals: 0 , pilars: 6} , igloo: {animals: 0}}    
+    return board
+end
+
+board = init
+dice = shoot
+begin
+    if dice == "bridge" 
+        move_field_bridge
+    elsif dice==pilars
+        remove_pilars
+    else
+        move_bridge_igloo
+    end
+end while (check_home board)=="relaunch"
+
+
 
