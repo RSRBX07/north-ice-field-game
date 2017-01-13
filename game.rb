50f2e77a8321312f485414bf03622ad4af40d5a1
neo972 <pablo972fox@gmail.com>
2017-01-13 15:28:12 +0100

    modif shoot

--- game.rb
+++ game.rb
@@ -14,11 +14,11 @@ finish="igloo"
 # Johann
 
 def shoot
-    dice = [ "igloo", "bridge", "ice", "igloo", "bridge", "ice"].shuffle
+    dice = [ "igloo", "bridge", "ice"].shuffle
     dice[0]
 end
 
-puts "Votre dé affiche: #{shoot}"
+
 
 # Patrick
 
