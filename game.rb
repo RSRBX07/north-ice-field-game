108e075c45efb98c9a6c286a38ca8af38a1a60ba
OUCHEM Ouissem <smartmen0909@gmail.com>
2017-01-13 11:54:40 +0100

    Update game.rb

--- game.rb
+++ game.rb
@@ -5,21 +5,3 @@
 # Ouissam
 
 # Johann
-
-# dice = {
-#     1: "igloo",
-#     2: "bridge",
-#     3: "ice",
-#     4: "igloo",
-#     5: "bridge",
-#     6: "ice",
-# }
-
-
-
-def shoot
-    dice = [ "igloo", "bridge", "ice", "igloo", "bridge", "ice"].shuffle
-    puts dice[0]
-end
-
-shoot
