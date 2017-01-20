907339a9b6b22ff2c94c03ee60fc50316f6061a2
hind <hind.lepert@cefimis.com>
2017-01-20 16:30:01 +0100

    add is_full

--- crow_game/field.rb
+++ crow_game/field.rb
@@ -3,26 +3,31 @@ class TreeSet
 
     def initialize
         @trees ={}
-        @trees[red: 4]
-        @trees[green: 4]
-        @trees[blue:  4]
-        @trees[yellow: 4]        
+        @trees[red: []] ; 4.times do @trees[:red].push Fruit.new "red" end
+        @trees[green: []] ; 4.times do @trees[:green].push Fruit.new "green" end
+        @trees[blue:  []] ; 4.times do @trees[:blue].push Fruit.new "blue" end
+        @trees[yellow: []] ; 4.times do @trees[:yellow].push Fruit.new "yellow" end       
     end     
     def fruit_remove color
-        return false if @trees[color.to_sym] = 0 
-        @trees[color.to_sym] -= 1         
+        return false if @trees[color.to_sym].length = 0 
+        @trees[color.to_sym].pop         
     end
 end
 
 class Basket
     attr_reader :gathed , :capacity
     def initialize value
-        @gathed = 0
+        @gathed =[]
         @capacity = value
     end
     
-    def gath_fruit
-        @gathed += 1
+    def gath_fruit fruit
+        @gathed.push fruit
+    end
+
+    def is_full?
+        return true if @gathed.length ==@capacity
+        return false
     end
 end
 
@@ -34,8 +39,8 @@ class Field
         @basket = Basket.new 16
     end 
 
-    def fruit_remove color
-       @tree_set.fruit_remove color
-       @basket.gath_fruit
+    def fruit_remove color       
+        fruit = @tree_set.fruit_remove color
+        @basket.gath_fruit fruit
     end   
 end
