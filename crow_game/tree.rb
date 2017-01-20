d64967d771badaf5fd2c783b8ae60a8131a40aab
OOuissem <smartmen0909@gmail.com>
2017-01-20 14:12:49 +0100

    adding Field, trees and basket Classes

---
+++ crow_game/tree.rb
@@ -0,0 +1,31 @@
+class TreeSet
+    attr_reader trees:
+
+    def initialize
+        trees ={}
+        trees[red: 4]
+        trees[green: 4]
+        trees[blue:  4]
+        trees[yellow: 4]        
+    end     
+    def treeRemove color
+        return false if trees[color.to_sym] = 0 
+        trees[color.to_sym] -= 1         
+    end
+end
+
+class Basket
+    attr_reader :number
+    def initialize
+        number = 0
+    end
+end
+
+class Field
+    attr_reader :tree_set , :basket 
+
+    def initialize
+        tree_set = TreeSet.new
+        basket = Basket.new
+    end    
+end
