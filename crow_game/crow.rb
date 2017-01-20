e36cb71d3a2d307c03d329893624965472945e67
neo972 <pablo972fox@gmail.com>
2017-01-20 12:27:20 +0100

    final work

--- crow_game/crow.rb
+++ crow_game/crow.rb
@@ -1,9 +1,21 @@
 class Crow
-
+  attr_reader :position
+  @state
   def initialize
-    @position = 1
+    position = 1
+    @state= "hungry"
   end
-  def move
-    @position += 1
+  def move    
+    @state = "eating" if position==4
+    position += 1
   end
+
+  
+  # def win
+  #   if @position == 5
+  #     puts "The crow win"
+  #   else
+  #     puts "The crow lose"
+  #   end
+  # end
 end
