598d342232b44fd0414bdfbbd7fbf0f4266198ff
hind <hind.lepert@cefimis.com>
2017-01-20 15:46:26 +0100

    add ArgumentError

--- crow_game/fruit.rb
+++ crow_game/fruit.rb
@@ -2,21 +2,22 @@ class Fruit
   attr_reader :cueilli
   attr_reader :color
   attr_reader :fruit_nombre
-  COLORS =[red, blue, yellow, green]
+  COLORS = [:red, :blue, :yellow, :green]
     
-  def initialize 
-    @fruit_nombre = 4
-    @color = COLORS[:red]
-    @cueilli = false
+  def initialize color_symbol
+    raise ArgumentError.new "new fruit color should be one of #{Fruit::COLORS}, got #{color_symbol}" unless Fruit::COLORS.include? color_symbol
 
     puts " le fruit n'est pas cueilli, il est toujours positionné sur l'arbre"
+    @cueilli = false
+    @color = color_symbol
   end
 
 
-  def decroche_toi (COLORS[0])
-    @color = 
+  def decroche_toi
+    @color = 55
     @fruit_nombre -= 1
     puts "le fruit est cueilli, et se trouve donc dans le panier"
 
   end
 end
+
