04cabe11150b8e3176087519126f5a8765559563
hind <hind.lepert@cefimis.com>
2017-01-20 14:22:40 +0100

    add rectifications cueilli

--- crow_game/fruit.rb
+++ crow_game/fruit.rb
@@ -1,15 +1,22 @@
 class Fruit
-  attr_reader :cueilli?
+  attr_reader :cueilli
   attr_reader :color
 end
 
-def decroche_toi(nouvelle_position)
-  @position = nouvelle_position
+def decroche_toi(color)
+  @color = color
+  @color -= 1
   puts "le fruit est cueilli, et se trouve donc dans le panier"
 
 end
 
 def initialize
-  @position = "arbre"
+  @color = 4
+  @cueilli = false
   puts " le fruit n'est pas cueilli, il est toujours positionné sur l'arbre"
+end
+
+def cueilli
+  @cueilli = true
+  puts "le fruit est cueilli"
 end
