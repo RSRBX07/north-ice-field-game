0821d6ec65539774c770c159b77a28b10a5ccff1
hind <hind.lepert@cefimis.com>
2017-01-20 14:24:58 +0100

    add private methods

--- crow_game/fruit.rb
+++ crow_game/fruit.rb
@@ -3,20 +3,22 @@ class Fruit
   attr_reader :color
 end
 
-def decroche_toi(color)
-  @color = color
-  @color -= 1
-  puts "le fruit est cueilli, et se trouve donc dans le panier"
 
-end
 
 def initialize
   @color = 4
   @cueilli = false
   puts " le fruit n'est pas cueilli, il est toujours positionné sur l'arbre"
 end
-
+private
 def cueilli
   @cueilli = true
   puts "le fruit est cueilli"
+end
+
+def decroche_toi(color)
+  @color = color
+  @color -= 1
+  puts "le fruit est cueilli, et se trouve donc dans le panier"
+
 end
