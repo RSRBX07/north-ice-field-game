1b5543b3eaafb2d9c1dca53d4ecf6d94987850d8
hind <hind.lepert@cefimis.com>
2017-01-20 12:16:48 +0100

    def methods move_fruit et initialize

--- crow_game/fruits.rb
+++ crow_game/fruits.rb
@@ -1,5 +1,14 @@
 class Fruit
+  attr_reader :name , :color , :nombre
+  attr_reader :position
+end
 
+def move_fruit(nouvelle_position)
+  @position = nouvelle_position
+  puts "le fruits est dans le panier"
 
+end
 
+def initialize
+  @position = "arbre"
 end
